dc6810254a963356bb1575fc25b3d380aff4fcb3
Travis Swicegood <development@domain51.com>
2013-12-18 12:01:31 -0600

    Proof-of-concept for turning a response into an API

--- sample/settings.py
+++ sample/settings.py
@@ -2,6 +2,8 @@
 import os
 
 DEBUG = os.environ.get('DEBUG', True)
+if type(DEBUG) is not bool:
+    DEBUG = DEBUG == 1
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -12,7 +14,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': './project.db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
@@ -24,7 +26,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', ]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -125,7 +127,16 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+
+    'newsapp',
 )
+
+if not DEBUG:
+    INSTALLED_APPS = ('prod', ) + INSTALLED_APPS
+
+# if DEBUG is False:
+#     INSTALLED_APPS = ('prod', ) + INSTALLED_APPS
+# INSTALLED_APPS += ('prod', )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
 
